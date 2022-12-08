d3062158c581486c18ca7ad410ab931ebd03cde5
isabellaodenthal23 <90466594+isabellaodenthal23@users.noreply.github.com>
2022-12-07 22:04:29 -0500

    almost done!
    - added note about Puerto Rico + District of Columbia on index.html
    - added micropage infographics
    - added another sentence to state.ejs comparing to national average
    - edited about.html to reflect what we actually ended up doing
    - added in note about data source + demographics (index.html)
    - deleted test index.html
    - just did some other edits! especially in about.html
    
    The only thing we still have to do is the build pipeline/prebuild: I am working on it! DONT RUN NPM RUN BUILD because maybe it will cause issues I'm not sure. Just be safe!!
    
    I also need to make sure that my edits will link to the github page so I will check that after I push.

--- Data-Journalism-Demo/Data-Journalism/src/prebuild.js
+++ Data-Journalism-Demo/Data-Journalism/src/prebuild.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
-fs.rmdirSync('../public');
-fs.mkdirSync('../public');
+fs.rmdirSync('public');
+fs.mkdirSync('public');
 
-fs.copyFileSync('styles.css', '../public');
-fs.copyFileSync('diya_prototype.jpg', '../public');
-fs.copyFileSync('isabella_macropage.jpg', '../public');
-fs.copyFileSync('isabella_micropage.jpg', '../public');
-fs.copyFileSync('project-pipeline.jpg', '../public');
+fs.copyFileSync('styles.css', 'public');
+fs.copyFileSync('diya_prototype.jpg', 'public');
+fs.copyFileSync('isabella_macropage.jpg', 'public');
+fs.copyFileSync('isabella_micropage.jpg', 'public');
+fs.copyFileSync('project-pipeline.jpg', 'public');
