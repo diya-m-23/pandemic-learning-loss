84c51457a91d3abe0fe364c3f0b33ed654eaea5e
isabellaodenthal23 <90466594+isabellaodenthal23@users.noreply.github.com>
2022-12-08 12:54:24 -0500

    final commit!
    -made prebuild work!!!
    - everything else is final!!!!!!
    
    I'm so happy and proud of us yay!!!!

--- Data-Journalism-Demo/Data-Journalism/src/prebuild.js
+++ Data-Journalism-Demo/Data-Journalism/src/prebuild.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
 
-fs.rmdirSync('public');
-fs.mkdirSync('public');
+fs.rmdirSync('../public', { recursive: true, force: true });
+fs.mkdirSync('../public'); //delete and remake public folder
 
-fs.copyFileSync('styles.css', 'public');
-fs.copyFileSync('diya_prototype.jpg', 'public');
-fs.copyFileSync('isabella_macropage.jpg', 'public');
-fs.copyFileSync('isabella_micropage.jpg', 'public');
-fs.copyFileSync('project-pipeline.jpg', 'public');
+fs.copyFileSync('styles.css', '../public/styles.css'); //move styles.css into public
+fs.copyFileSync('../diya-prototype.jpg', '../public/diya-prototype.jpg');
+fs.copyFileSync('../isabella-macropage.jpg', '../public/isabella-macropage.jpg');
+fs.copyFileSync('../isabella-micropage.jpg', '../public/isabella-micropage.jpg');
+fs.copyFileSync('../project-pipeline.jpg', '../public/project-pipeline.jpg'); //move images into public
+
+fs.mkdirSync('../public/data');
+fs.copyFileSync('../data/generateJSON.js', '../public/data/generateJSON.js');
+fs.copyFileSync('../data/scores.csv', '../public/data/scores.csv');
+fs.copyFileSync('../data/scores.json', '../public/data/scores.json');
+//copy sync data folder into public -- generateJSON, scores.csv, scores.json
